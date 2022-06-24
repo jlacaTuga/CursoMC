1511ed20e980a4f13365d86617579676885296cb
jlaca <jlaca@live.com.pt>
2022-06-24 20:50:33 -0300

    Terceiro  - já tá mapeando

--- src/main/java/com/tuga/cursomc/CursoMcApplication.java
+++ src/main/java/com/tuga/cursomc/CursoMcApplication.java
@@ -2,8 +2,10 @@ package com.tuga.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"resources"})
 public class CursoMcApplication {
 
     public static void main(String[] args) {
